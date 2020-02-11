987eb0e2ec8ce61ddc64c6ba009bbfc14f7086bf
marcinurbaniak <marcin.urbaniak@gmail.com>
2020-02-11 16:36:59 +0100

    create api request and response class for UserAconut; create UserAccountController and first model class UserAccount

---
+++ src/main/java/com/example/microloans/api/response/CreateUserAccountResponse.java
@@ -0,0 +1,22 @@
+package com.example.microloans.api.response;
+
+public class CreateUserAccountResponse  extends BasicResponse{
+
+    private Long userId;
+
+    public CreateUserAccountResponse() {
+    }
+
+    public CreateUserAccountResponse(String responseMsg, Long userId) {
+        super(responseMsg);
+        this.userId = userId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}

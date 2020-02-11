14b35118f9b5013282a7f423f83d0b2ce766801a
marcinurbaniak <marcin.urbaniak@gmail.com>
2020-02-11 14:49:09 +0100

    initial commit

---
+++ src/main/java/com/example/microloans/MicroLoansApplication.java
@@ -0,0 +1,13 @@
+package com.example.microloans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroLoansApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroLoansApplication.class, args);
+    }
+
+}

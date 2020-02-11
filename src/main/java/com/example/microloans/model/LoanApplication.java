0bcce1008752e4669eb9ca75bf8b11b5af2e18f3
marcinurbaniak <marcin.urbaniak@gmail.com>
2020-02-11 19:19:19 +0100

    correct entity LoanApplication

--- src/main/java/com/example/microloans/model/LoanApplication.java
+++ src/main/java/com/example/microloans/model/LoanApplication.java
@@ -13,15 +13,58 @@ public class LoanApplication {
     private InetAddress ipAddress;
     private LocalDate applicationDay;
     private LocalTime applicationTime;
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "user_id", referencedColumnName = "id")
     private UserAccount userAccount;
     @OneToOne
     @JoinColumn(name = "loan_id", referencedColumnName = "id")
     private Loan loan;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public InetAddress getIpAddress() {
+        return ipAddress;
+    }
 
+    public void setIpAddress(InetAddress ipAddress) {
+        this.ipAddress = ipAddress;
+    }
 
+    public LocalDate getApplicationDay() {
+        return applicationDay;
+    }
+
+    public void setApplicationDay(LocalDate applicationDay) {
+        this.applicationDay = applicationDay;
+    }
+
+    public LocalTime getApplicationTime() {
+        return applicationTime;
+    }
+
+    public void setApplicationTime(LocalTime applicationTime) {
+        this.applicationTime = applicationTime;
+    }
+
+    public UserAccount getUserAccount() {
+        return userAccount;
+    }
+
+    public void setUserAccount(UserAccount userAccount) {
+        this.userAccount = userAccount;
+    }
+
+    public Loan getLoan() {
+        return loan;
+    }
+
+    public void setLoan(Loan loan) {
+        this.loan = loan;
+    }
 }
